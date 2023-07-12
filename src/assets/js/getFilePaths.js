2f8c182a8c369dd2c24571fdae5558ac4cdb5b7d
Bruno <github@bbergmann.de>
2023-07-12 22:01:29 +0000

    Ignore files with dot prefix

--- src/assets/js/getFilePaths.js
+++ src/assets/js/getFilePaths.js
@@ -4,18 +4,21 @@ const fs = require('fs');
 const path = require('path');
 
 function getFilesWithAbsolutePaths(folderPath, callback) {
-    fs.readdir(folderPath, (err, files) => { // read directory
-      if (err) { // catch errors
-        callback(err, null);
-        return;
-      }
-  
-      // map absolute path to the filenames
-      const absolutePaths = files.map(file => path.join(folderPath, file));
-  
-      // return array of filepaths
-      callback(null, absolutePaths);
-    });
-  }
+  fs.readdir(folderPath, (err, files) => { // read directory
+    if (err) { // catch errors
+      callback(err, null);
+      return;
+    }
 
-  module.exports = getFilesWithAbsolutePaths;
+    // filter out files starting with a dot
+    const filteredFiles = files.filter(file => !file.startsWith('.'));
+    
+    // map absolute path to the filenames
+    const absolutePaths = filteredFiles.map(file => path.join(folderPath, file));
+
+    // return array of filepaths
+    callback(null, absolutePaths);
+  });
+}
+
+module.exports = getFilesWithAbsolutePaths;
